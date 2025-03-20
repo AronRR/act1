c5be7e59320c420805b6a895fd8a4eaf3b1f11c0
aislinn <a01666225@tec.mx>
2025-03-20 13:41:50 -0600

    Función círculo con cambio y color con Y

--- paint.py
+++ paint.py
@@ -39,11 +39,12 @@ def square(start, end):
 def circle(start, end):
     """Draw circle from start to end."""	
     up()
-    goto(start.x, start.y)
+    goto(start.x, start.y- ((end.x -start.x)** 2 +(end.y -start.y)**2)** 0.5)
     down()
     begin_fill()
+
     radius = ((end.x - start.x) ** 2 + (end.y - start.y) ** 2 ) **0.5
-    circle(radius)
+    turtle.circle(radius)
     end_fill()	
 
 
